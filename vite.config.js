5a2cc3fb6750d363d19ab3b04c0c22a51fae4915
a-hui <hui760419@gmail.com>
2025-12-01 17:51:40 +0800

    Setup GitHub Pages with docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 這裡一定要寫成 /你的 repo 名稱/
+  base: '/goodkit-frontEnd/',
+  build: {
+    // 叫 Vite 把 build 結果輸出到 docs 資料夾
+    outDir: 'docs',
+  },
 })
